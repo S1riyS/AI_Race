dadbb677e4baaf25bcff94ee13e672ed922b089f
kiril <kirill.ankudinov.94@mail.ru>
2023-01-07 13:54:27 +0500

    FEATURE(camera): added optimization (only objects within camera view are drawn)

--- src/ai_race/camera.py
+++ src/ai_race/camera.py
@@ -5,6 +5,7 @@ class Camera(pygame.sprite.Group):
     def __init__(self):
         super().__init__()
         self.display_surface = pygame.display.get_surface()
+        self.screen_width, self.screen_height = self.display_surface.get_size()
         self.offset = pygame.math.Vector2()
 
         self.half_width = self.display_surface.get_size()[0] // 2
@@ -19,4 +20,9 @@ class Camera(pygame.sprite.Group):
 
         for sprite in self.sprites():
             offset_pos = sprite.rect.topleft - self.offset
-            self.display_surface.blit(sprite.image, offset_pos)
+
+            fits_width = -0.3 * self.screen_width <= offset_pos.x <= 1.3 * self.screen_width
+            fits_height = -0.3 * self.screen_height <= offset_pos.y <= 1.3 * self.screen_height
+
+            if fits_width and fits_height:
+                self.display_surface.blit(sprite.image, offset_pos)
